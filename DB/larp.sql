46ff5e33eff715adfccce8be6dbcece55ae284c2
Greg Kraehenbuehl <chioxin@gmail.com>
2019-03-08 11:30:38 -0700

    updating Database

--- DB/larp.sql
+++ DB/larp.sql
@@ -16,18 +16,286 @@ CREATE SCHEMA IF NOT EXISTS `larpdb` DEFAULT CHARACTER SET utf8 ;
 USE `larpdb` ;
 
 -- -----------------------------------------------------
+-- Table `picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `picture` ;
+
+CREATE TABLE IF NOT EXISTS `picture` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `url` TEXT NOT NULL,
+  `alt` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(50) NOT NULL,
+  `zipcode` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(20) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nickname` VARCHAR(50) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
   `password` VARCHAR(200) NOT NULL,
-  `email` VARCHAR(100) NULL,
-  `role` VARCHAR(45) NULL,
-  `active` TINYINT NULL,
-  PRIMARY KEY (`id`))
+  `email` VARCHAR(100) NOT NULL,
+  `role` VARCHAR(15) NOT NULL DEFAULT 'user',
+  `birth_date` DATE NOT NULL,
+  `picture_id` INT UNSIGNED NOT NULL,
+  `address_id` INT UNSIGNED NOT NULL,
+  `create_date` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_picture1_idx` (`picture_id` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  UNIQUE INDEX `username_UNIQUE` (`nickname` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  CONSTRAINT `fk_user_picture1`
+    FOREIGN KEY (`picture_id`)
+    REFERENCES `picture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `genre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `genre` ;
+
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `picture_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_genre_picture1_idx` (`picture_id` ASC),
+  CONSTRAINT `fk_genre_picture1`
+    FOREIGN KEY (`picture_id`)
+    REFERENCES `picture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `story`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `story` ;
+
+CREATE TABLE IF NOT EXISTS `story` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NULL,
+  `address_id` INT UNSIGNED NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `genre_id` INT UNSIGNED NULL,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_story_address1_idx` (`address_id` ASC),
+  INDEX `fk_story_user1_idx` (`user_id` ASC),
+  INDEX `fk_story_genre1_idx` (`genre_id` ASC),
+  CONSTRAINT `fk_story_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_story_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_story_genre1`
+    FOREIGN KEY (`genre_id`)
+    REFERENCES `genre` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event` ;
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NULL,
+  `date` DATETIME NOT NULL,
+  `story_id` INT UNSIGNED NOT NULL,
+  `address_id` INT UNSIGNED NOT NULL,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_story1_idx` (`story_id` ASC),
+  INDEX `fk_event_address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_event_story1`
+    FOREIGN KEY (`story_id`)
+    REFERENCES `story` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_user` ;
+
+CREATE TABLE IF NOT EXISTS `event_user` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NULL,
+  `user_id` INT UNSIGNED NULL,
+  `event_id` INT UNSIGNED NOT NULL,
+  `picture_id` INT UNSIGNED NOT NULL,
+  `signup_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_cast_event1_idx` (`event_id` ASC),
+  INDEX `fk_event_user_user1_idx` (`user_id` ASC),
+  INDEX `fk_event_user_picture1_idx` (`picture_id` ASC),
+  CONSTRAINT `fk_cast_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_user_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_user_picture1`
+    FOREIGN KEY (`picture_id`)
+    REFERENCES `picture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_comment` ;
+
+CREATE TABLE IF NOT EXISTS `event_comment` (
+  `id` INT UNSIGNED NOT NULL,
+  `comment` TEXT NOT NULL,
+  `post_time` DATETIME NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `event_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_comment_event1_idx` (`event_id` ASC),
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `story_picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `story_picture` ;
+
+CREATE TABLE IF NOT EXISTS `story_picture` (
+  `picture_id` INT UNSIGNED NOT NULL,
+  `story_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`picture_id`, `story_id`),
+  INDEX `fk_picture_has_story_story1_idx` (`story_id` ASC),
+  INDEX `fk_picture_has_story_picture1_idx` (`picture_id` ASC),
+  CONSTRAINT `fk_picture_has_story_picture1`
+    FOREIGN KEY (`picture_id`)
+    REFERENCES `picture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_picture_has_story_story1`
+    FOREIGN KEY (`story_id`)
+    REFERENCES `story` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_picture` ;
+
+CREATE TABLE IF NOT EXISTS `event_picture` (
+  `event_id` INT UNSIGNED NOT NULL,
+  `picture_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`event_id`, `picture_id`),
+  INDEX `fk_event_has_picture_picture1_idx` (`picture_id` ASC),
+  INDEX `fk_event_has_picture_event1_idx` (`event_id` ASC),
+  CONSTRAINT `fk_event_has_picture_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_has_picture_picture1`
+    FOREIGN KEY (`picture_id`)
+    REFERENCES `picture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `story_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `story_comment` ;
+
+CREATE TABLE IF NOT EXISTS `story_comment` (
+  `id` INT UNSIGNED NOT NULL,
+  `comment` TEXT NOT NULL,
+  `post_time` DATETIME NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `story_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_comment_event10_idx` (`story_id` ASC),
+  CONSTRAINT `fk_comment_user10`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_event10`
+    FOREIGN KEY (`story_id`)
+    REFERENCES `story` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
