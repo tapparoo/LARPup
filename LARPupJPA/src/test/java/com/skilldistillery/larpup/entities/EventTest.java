054007ca7f6c21de0a30062736158db5aea6c608
Yang Chen <yang.cl7@gmail.com>
2019-03-08 16:24:25 -0700

    remove deleteme, created all junit stubs

---
+++ LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/EventTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.larpup.entities;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("event tests")
+class EventTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Event event;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("LARPupJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		event = em.find(Event.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		event = null;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
