aa154e5fe17568723ca708a65c9ca554ac6b4630
Yang Chen <yang.cl7@gmail.com>
2019-03-08 17:55:23 -0700

    Added EventUserInfo, and update @Table

--- LARPupJPA/src/main/java/com/skilldistillery/larpup/entities/EventUser.java
+++ LARPupJPA/src/main/java/com/skilldistillery/larpup/entities/EventUser.java
@@ -9,10 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
+@Table(name="event_user")
 public class EventUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
