a77b8cff958118d43c5db1ce69e599d6f7ffe7c7
Yang Chen <yang.cl7@gmail.com>
2019-03-09 13:36:18 -0700

    Created and tested Event user info test

--- LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/EventUserInfoTest.java
+++ LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/EventUserInfoTest.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.larpup.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.persistence.EntityManager;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -41,7 +43,22 @@ class EventUserInfoTest {
 		em.close();
 		eventUserInfo = null;
 	}
-
+	@Test
+	void test_event_user_name_des_storyid_mapping() {
+		assertEquals("Thrall", eventUserInfo.getName());
+		assertEquals("An orc, that wants a human wife, for whatever reason.", eventUserInfo.getDescription());
+		assertEquals(2,eventUserInfo.getStory().getId());
+		
+	}
+	@Test
+	void test_eventuserinfo_manytoOne_mapping() {
+		assertEquals(2, eventUserInfo.getStory().getId());
+		assertEquals("World of Warcraft", eventUserInfo.getStory().getName());
+		assertEquals("An epic story about Thrall falling in love with a human.", eventUserInfo.getStory().getDescription());
+	}
+	
+	
+	@Disabled
 	@Test
 	void test() {
 		fail("Not yet implemented");
