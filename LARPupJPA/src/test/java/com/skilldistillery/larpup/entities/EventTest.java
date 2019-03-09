fe325ffe3d4fb1d488ccdcd506ef20781e290043
Yang Chen <yang.cl7@gmail.com>
2019-03-09 11:37:09 -0700

    Update EventTest with two tests

--- LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/EventTest.java
+++ LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/EventTest.java
@@ -1,6 +1,9 @@
 package com.skilldistillery.larpup.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -40,6 +43,28 @@ class EventTest {
 	void tearDown() throws Exception {
 		em.close();
 		event = null;
+	}
+	@Test
+	void test_name_for_event_test_mapping() {
+		assertEquals("Train Heist", event.getName());
+		
+	}
+	
+	@Test
+	void test_event_test_id6_is_null() {
+		event = em.find(Event.class, 5);
+		assertEquals("Marriage of Two Races", event.getName());
+		assertEquals("Thrall and Aggra finally get together to have a marriage to bring two kingdoms together.",event.getDescription());
+		assertEquals("2018-07-15T23:57:41", event.getDate().toString());
+		assertEquals(2,event.getStory().getId());
+		assertEquals(5, event.getAddress().getId());
+	}
+//	'4','18300 W Alameda Pkwy','Lakewood','Colorado','80465','7208652492'
+
+	@Test
+	void test_manytoone_association_between_event_and_address() {
+		assertEquals("18300 W Alameda Pkwy", event.getAddress().getStreet());
+		
 	}
 
 	@Test
