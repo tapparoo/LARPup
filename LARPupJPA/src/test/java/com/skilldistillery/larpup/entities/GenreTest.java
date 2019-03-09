5eb17da330fbfede8391e950bdd64b66164cd574
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-03-09 12:51:01 -0700

    Finished Genre entity tests

--- LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/GenreTest.java
+++ LARPupJPA/src/test/java/com/skilldistillery/larpup/entities/GenreTest.java
@@ -1,15 +1,19 @@
 package com.skilldistillery.larpup.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,6 +46,22 @@ class GenreTest {
 		genre = null;
 	}
 
+//	'1','Space Operah','4'
+	@Test
+	void test_genre_entity_mappings() {
+		assertEquals("Space Operah", genre.getName());
+	}
+	
+//	@OneToOne
+//	@JoinColumn(name="picture_id")
+//	private Picture picture;
+	@Test
+	void test_genre_picture_association() {
+		assertEquals("https://starwarsblog.starwars.com/wp-content/uploads/2018/08/battlefront-2-clone-troopers-tall.jpg", genre.getPicture().getUrl());
+		assertEquals("genre image", genre.getPicture().getAlt());
+	}
+	
+	@Disabled
 	@Test
 	void test() {
 		fail("Not yet implemented");
