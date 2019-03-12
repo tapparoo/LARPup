48fe64d6c0d08a91c44ad277de69161adbf73eeb
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-03-11 19:20:43 -0600

    Update event no longer creates a new address

--- LARPup/src/main/java/com/skilldistillery/larpup/data/EventDTO.java
+++ LARPup/src/main/java/com/skilldistillery/larpup/data/EventDTO.java
@@ -1,9 +1,7 @@
 package com.skilldistillery.larpup.data;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
